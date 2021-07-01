99ef31ef81ee44c9f7fb238c2b42fdcb855de5c7
mammuttaqin <muttaqinimam76@gmail.com>
2021-07-01 15:19:50 +0700

    Add Routes

---
+++ controllers/controller.js
@@ -0,0 +1,15 @@
+class Controller {
+    static loginPage(req, res){
+
+    }
+
+    static registerForm(req, res){
+
+    }
+
+    static registerSave(req, res){
+        
+    }
+}
+
+module.exports = Controller
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const router = express.Router();
+const Controller = require("../controllers/controller");
 
+router.get("/login", Controller.loginPage);
 
+router.get("/register", Controller.registerForm);
+
+router.post("/register", Controller.registerSave)
 
 
 
