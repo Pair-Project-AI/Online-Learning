c3a016dcba7cd4c415f7141ea8d7e263867ae5d7
mammuttaqin <muttaqinimam76@gmail.com>
2021-07-02 08:26:36 +0700

    Deploy Heroku

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const session = require("express-session");
 const app = express();
-const port = 3000;
+const port = process.env.PORT;
 const routes = require("./routes");
 
 app.set('view engine', 'ejs');
